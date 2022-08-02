def188052a2767a89f30a5939ec0784d0651c3de
Realizedd <TheMCman12@gmail.com>
2022-08-02 23:50:07 +0900

    8/2 progress

--- src/level4/Level4Q6.java
+++ src/level4/Level4Q6.java
@@ -2,27 +2,42 @@ package level4;
 
 public class Level4Q6 {
 
-    private int maxScore = 0;
+    private int maxPossibleScore;
+    private boolean found = false;
+    private int currentMaxScore = 0;
 
     public int solution(int[] left, int[] right) {
-        iterate(left, right, 0, 0, 0);
-        int score = maxScore;
-        maxScore = 0;
+        maxPossibleScore = java.util.Arrays.stream(right).sum();
+        iterate(left, right, 0, maxPossibleScore, 0, 0);
+        int score = currentMaxScore;
+        this.currentMaxScore = 0;
         return score;
     }
 
-    private void iterate(int[] left, int[] right, int score, int li, int ri) {
+    private void iterate(int[] left, int[] right, int score, int maxPossibleScore, int li, int ri) {
+        if (found) {
+            return;
+        }
+
         if (li == left.length || ri == right.length) {
-            maxScore = Math.max(maxScore, score);
+            if (score == this.maxPossibleScore) {
+                found = true;
+            }
+
+            currentMaxScore = Math.max(currentMaxScore, score);
+            return;
+        }
+
+        if (score + maxPossibleScore < this.currentMaxScore) {
             return;
         }
 
         if (left[li] > right[ri]) {
-            iterate(left, right, score + right[ri], li, ri + 1);
-        } else if (left[li] == right[ri]) {
-            iterate(left, right, score, li + 1, ri + 1);
-        } else {
-            iterate(left, right, score, li + 1, ri);
+            iterate(left, right, score + right[ri], maxPossibleScore - right[ri], li, ri + 1);
+            return;
         }
+        
+        iterate(left, right, score, maxPossibleScore - right[ri], li + 1, ri + 1);
+        iterate(left, right, score, maxPossibleScore, li + 1, ri);
     }
 }
